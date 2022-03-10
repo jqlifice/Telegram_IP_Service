d851d8b056952024e7fb37040ae67c4b5e49a518
jqlifice <jqlifice@gmail.com>
2022-03-10 11:17:10 +0100

    Bot v 1.0

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
-const Telegraf = require('telegraf');
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+const http = require('http');
 
+const bot = new Telegraf(process.env.Token);
+let myIP = undefined;
+
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+    resp.on('data', function(ip) {
+        myIP = ip;
+    });
+});
+
+let checkForValidUsers = false;
+let validUsers;
+if(process.env.AllowedUsers){
+    checkForValidUsers = true;
+    validUsers = process.env.AllowedUsers.split(",");
+    console.log(validUsers);
+}
+
+
+
+bot.start((ctx) =>{
+   ctx.reply('Hello');
+});
+bot.command('ping', (ctx) => {
+    if(checkForValidUsers && validUsers.indexOf(ctx.update.message.from.username) > -1){
+        ctx.reply(`Bot currently operated from public IP-Adress ${myIP}`);
+    }else {
+        ctx.reply("pong");
+    }
+});
+
+bot.launch();
---
+++ readme.md
@@ -0,0 +1,9 @@
+###Configuration:
+1. Create a [.env](https://www.npmjs.com/package/dotenv) file in the root directory of the bot.
+2. Add your Bot Token to the .env File. (Token=yourToken) (If you don't know how to create a Telegram Bot more information can be found [here](https://core.telegram.org/bots).) \
+3. If you want to limit the users that can access this Bot also add AllowedUsers=name1,name2,... to your .env file.
+
+
+\
+After that simply run the bot using
+>Node main.js
